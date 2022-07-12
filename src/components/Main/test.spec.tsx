6c036b0f86186512f58c56d13dd55c8d7bcb9cf2
FranLyrio <lyriofranciele@gmail.com>
2022-07-12 14:46:19 -0300

    refactor: corrige testes

--- src/components/Main/test.spec.tsx
+++ src/components/Main/test.spec.tsx
@@ -1,20 +1,24 @@
-import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 
 import Main from '.';
 
+const props = {
+	title: 'Title',
+	description: 'Description'
+};
+
 describe('<Main />', () => {
 	it('should render the heading', () => {
-		const { container } = render(<Main />);
+		const { container } = render(<Main {...props} />);
 
-		const heading = screen.getByRole('heading', { name: /react avançado/i });
+		const heading = screen.getByRole('heading', { name: props.title });
 
 		expect(heading).toBeInTheDocument();
 		expect(container.firstChild).toMatchSnapshot();
 	});
 
 	it('should render the colors correctly', () => {
-		const { container } = render(<Main />);
+		const { container } = render(<Main {...props} />);
 
 		expect(container.firstChild).toHaveStyle({ backgroundColor: '#06092b' });
 	});
