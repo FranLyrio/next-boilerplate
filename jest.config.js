369394535c9f1e7d06c28364bf875d7f0d67ffb7
FranLyrio <lyriofranciele@gmail.com>
2022-07-12 20:03:18 -0300

    chore: apaga arquivo babel

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-	testEnvironment: 'jsdom',
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	dir: './'
+});
+
+const customJestConfig = {
+	moduleDirectories: ['node_modules', '<rootDir>/'],
+	testEnvironment: 'jest-environment-jsdom',
 	testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 	collectCoverage: true,
 	collectCoverageFrom: [
@@ -10,3 +18,5 @@ module.exports = {
 	],
 	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 };
+
+module.exports = createJestConfig(customJestConfig);
