9e79894b9b03705f49614374bf2b193cf5e7aaa2
FranLyrio <lyriofranciele@gmail.com>
2022-07-12 14:32:48 -0300

    chore: modifica importação de biblioteca

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-import withPWA from 'next-pwa';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
